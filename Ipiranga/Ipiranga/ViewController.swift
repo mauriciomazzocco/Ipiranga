fff2183468e70d51e509f0a52354c0e218b2e73e
Mauricio Balena Mazzocco <mauricio.balena@totvs.com.br>
2018-12-05 17:34:51 -0200

    Projeto padrao  apple Iniciado

---
+++ Ipiranga/Ipiranga/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ipiranga
+//
+//  Created by Maurício Mazzocco on 05/12/18.
+//  Copyright © 2018 Totvs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
